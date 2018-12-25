da60ad8a04ab9a66f9c5a396f817343bdf97cb76
Starzeco <marcel.k@onet.eu>
2018-12-25 21:47:22 +0100

    directives added to course project

--- AngularAssigments/directives-start/directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ AngularAssigments/directives-start/directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,14 +1,26 @@
-import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor = 'transparent';
+  @Input() highlightColor = 'yellow';
+  @HostBinding('style.backgroundColor') backgroundColor: string;    // Inny sposob na ustawianie stylu, zamiast renderer2
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'yellow');
-    this.renderer.createComment('lol jakis kom');
-    this.renderer.createText('cos tam');
+   // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'yellow');
+    this.backgroundColor = this.defaultColor;
   }
+  @HostListener('mouseenter') mouseOver(eventData: Event) {   // Nazwa mouseenter jest wymagana
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'yellow');
+    this.backgroundColor = this.highlightColor;
+  }
+  // Tak samo mouseleave tez jest wymagana
+  @HostListener('mouseleave') mouseLeave(eventData: Event) {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = this.defaultColor;
+  }
+
 }
