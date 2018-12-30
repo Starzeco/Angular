4641f570988b375a81151b7e9d2cb7c108cf9ebb
Starzeco <marcel.k@onet.eu>
2018-12-30 18:36:55 +0100

    Kilka zmian

--- AngularAssigments/routing-start/routing-start/src/app/app-routing.module.ts
+++ AngularAssigments/routing-start/routing-start/src/app/app-routing.module.ts
@@ -8,6 +8,9 @@ import {ServersComponent} from "./servers/servers.component";
 import {ServerComponent} from "./servers/server/server.component";
 import {EditServerComponent} from "./servers/edit-server/edit-server.component";
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import {AuthGuard} from "./auth-guard.service";
+import {CanDeactivateGuard} from "./servers/edit-server/can-deactivate-guard.service";
+import {ErrorPageComponent} from "./error-page/error-page.component";
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -15,11 +18,16 @@ const appRoutes: Routes = [
       { path: ':id/:name', component: UserComponent },
     ]},
 
-  { path: 'servers', component: ServersComponent, children: [
+  { path: 'servers',
+    //canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
+    component: ServersComponent,
+    children: [
       { path: ':id', component: ServerComponent },
-      { path: ':id/edit', component: EditServerComponent },
+      { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] },
     ]},
-  { path: 'not-found', component: PageNotFoundComponent },
+  /*{ path: 'not-found', component: PageNotFoundComponent },*/
+  { path: 'not-found', component: ErrorPageComponent, data: {message: 'Page not found!'}},
   { path: '**', redirectTo: 'not-found' },  // TO MUSI BYC NA KONCU Bo to jest jak dopasowanie do wzorca ** dostosuje sie do wszystkiego
 ];
 
--- AngularAssigments/routing-start/routing-start/src/app/app.module.ts
+++ AngularAssigments/routing-start/routing-start/src/app/app.module.ts
@@ -14,6 +14,10 @@ import { ServersService } from './servers/servers.service';
 
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import {AppRoutingModule} from "./app-routing.module";
+import {AuthGuard} from "./auth-guard.service";
+import {AuthService} from "./auth.service";
+import {CanDeactivateGuard} from "./servers/edit-server/can-deactivate-guard.service";
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 
 @NgModule({
@@ -25,15 +29,17 @@ import {AppRoutingModule} from "./app-routing.module";
     UserComponent,
     EditServerComponent,
     ServerComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    ErrorPageComponent
   ],
+
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     AppRoutingModule,
   ],
-  providers: [ServersService],
+  providers: [ServersService, AuthGuard, AuthService, CanDeactivateGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
